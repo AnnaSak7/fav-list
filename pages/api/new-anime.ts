b54f3fc5238f6d541a46ac14e1bafe4c5163a1f1
Anna Sakurai <zenasak@gmail.com>
2022-04-17 12:34:41 +0200

    fix api files

--- pages/api/new-anime.ts
+++ pages/api/new-anime.ts
@@ -1,14 +1,21 @@
 //  /api/new-anime
 // This is a secure place to store credentials. T∂he code defines here will never end oup on the client side.
 
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
 import { MongoClient } from "mongodb";
-import { NextApiRequest, NextApiResponse } from "next";
 import { runInNewContext } from "vm";
 
 const MONGO_URI:any = process.env.MONGO_URI
 
+type Data = {
+  name: string
+}
+
 // POST /api/new-anime
-const handler = async (req:NextApiRequest, res:NextApiResponse) =>{
+const handler = async (  req: NextApiRequest,
+  res: NextApiResponse<Data>) =>{
     try{
         if(req.method === 'POST'){
             const data = req.body;
